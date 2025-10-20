10046f5204863d67a001994c681eab17d6f8a8dd
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-20 18:04:02 +0300

    Select only needed info for furniture when rendering the catalog data

--- api/src/services/furnitureService.js
+++ api/src/services/furnitureService.js
@@ -1,6 +1,13 @@
 import Furniture from "../models/Furniture.js";
 
 export default {
+  getAll(){
+    return Furniture.find().select({
+      description: true,
+      price: true,
+      img: true,
+    });
+  },
   create(furnitureData) {
     return Furniture.create(furnitureData);
   },
