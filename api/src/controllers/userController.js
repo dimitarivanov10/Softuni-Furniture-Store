d14fa1d627794f4096f7346b1cd678fbb35eae00
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-20 15:22:12 +0300

    Add user controller

---
+++ api/src/controllers/userController.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const userController = Router();
+
+userController.post("/register", (req, res) => {
+  res.end();
+});
+
+export default userController;
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import furnitureController from "./controllers/furnitureController.js";
+import userController from "./controllers/userController.js";
 
 const routes = Router();
 
 routes.use("/data/catalog", furnitureController);
+routes.use("/user", userController);
 
 export default routes;
