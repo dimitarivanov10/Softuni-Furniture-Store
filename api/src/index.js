3d4c24d60a1bab5845cdb33e27d52ab72b055091
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-20 15:14:45 +0300

    Add furniture controller

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(cors());
-
+app.use(routes);
 app.get("/", (req, res) => {
   console.log("Working fine asl");
   res.send("Working fine ash");
