97b75c0de7c139bd58229c04bacfd23d88d24207
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-20 18:08:32 +0300

    Add furniture details

--- api/src/services/furnitureService.js
+++ api/src/services/furnitureService.js
@@ -1,12 +1,15 @@
 import Furniture from "../models/Furniture.js";
 
 export default {
-  getAll(){
+  getAll() {
     return Furniture.find().select({
       description: true,
       price: true,
       img: true,
     });
+  },
+  getOne(furnitureId) {
+    return Furniture.findById(furnitureId);
   },
   create(furnitureData) {
     return Furniture.create(furnitureData);
