7845f630eacb9928127b2382f223d8687798223a
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-20 14:04:33 +0300

    Add express to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
-console.log("Working");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Working fine asl");
+  res.send("Working fine ash");
+});
+
+app.listen(3030, () =>
+  console.log("Server is listenin on http://localhost:3030...")
+);
