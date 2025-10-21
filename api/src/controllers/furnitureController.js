8ab1ad2529fea80821dae57bb07dfc73415daafb
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-21 15:23:22 +0300

    Add error handliung

--- api/src/controllers/furnitureController.js
+++ api/src/controllers/furnitureController.js
@@ -1,6 +1,7 @@
 import querystring from "querystring";
 import { Router } from "express";
 import furnitureService from "../services/furnitureService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const furnitureController = Router();
 
@@ -27,9 +28,12 @@ furnitureController.post("/", async (req, res) => {
   const furnitureData = req.body;
   const ownerId = req.user.id;
 
-  const furniture = await furnitureService.create(furnitureData, ownerId);
-
-  res.status(201).json(furniture);
+  try {
+    const furniture = await furnitureService.create(furnitureData, ownerId);
+    res.status(201).json(furniture);
+  } catch (error) {
+    res.status(400).json({ message: getErrorMessage(error) });
+  }
 });
 
 furnitureController.put("/:furnitureId", async (req, res) => {
